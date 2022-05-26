75ede4a642acec17d4d9f05781049e00756f6392
E-Trinh <ethantrinh@csus.edu>
2022-05-26 15:50:02 -0700

    Added button to redraw the grid with different rows and columns

--- sketch.js
+++ sketch.js
@@ -1,18 +1,38 @@
-const container = document.createElement("div");
-container.classList.toggle("container");
+//Adds a grid of squares to the HTML file based on the number inputted
+function addGrid(count) {
+    const container = document.createElement("div");
+    container.classList.toggle("container");
 
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement("div");
-    row.classList.toggle("row");
-    for (let i = 0; i < 16; i++) {
-        const box = document.createElement("box");
-        box.addEventListener("mouseover", (e) => {
-            e.target.style.backgroundColor = "black";
-        });
-        box.classList.toggle("box")
-        row.appendChild(box);
+    for (let i = 0; i < count; i++) {
+        const row = document.createElement("div");
+        row.classList.toggle("row");
+        for (let i = 0; i < count; i++) {
+            const box = document.createElement("box");
+            box.addEventListener("mouseover", (e) => {
+                e.target.style.backgroundColor = "black";
+            });
+            box.classList.toggle("box")
+            row.appendChild(box);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
+    document.querySelector("body").appendChild(container);
 }
 
-document.querySelector("body").appendChild(container);
+//create a button for redrawing the grid with a different number of rows and columns
+const button = document.createElement("button");
+button.textContent = "Change Grid";
+button.classList.toggle("button");
+button.addEventListener("click", () => {
+    const count = prompt("How many rows and columns do you want for the square grid? Enter the number of columns or rows as a number. Max: 100");
+    if (count <= 100 && count > 0) {
+        document.querySelector("body").removeChild(document.querySelector(".container"));
+        addGrid(count);
+    } else {
+        alert("Invalid Number");
+    }
+});
+document.querySelector("body").appendChild(button);
+
+//draws the initial gridd
+addGrid(16);
