47f44c49b3ab61ba50c6aa899caca42a1b643c1f
E-Trinh <ethantrinh@csus.edu>
2022-05-26 15:35:31 -0700

    Adds grid of squares

---
+++ sketch.js
@@ -0,0 +1,18 @@
+const container = document.createElement("div");
+container.classList.toggle("container");
+
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement("div");
+    row.classList.toggle("row");
+    for (let i = 0; i < 16; i++) {
+        const box = document.createElement("box");
+        box.addEventListener("mouseover", (e) => {
+            e.target.style.backgroundColor = "green";
+        });
+        box.classList.toggle("box")
+        row.appendChild(box);
+    }
+    container.appendChild(row);
+}
+
+document.querySelector("body").appendChild(container);
